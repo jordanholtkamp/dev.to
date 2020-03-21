beca410e4bfe91f7a37b8d5a1ef0a7b5f1ec0602
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-21 16:00:28 -0600

    annotations for digest mailer

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -2,10 +2,15 @@ class DigestMailer < ApplicationMailer
   default from: -> { "DEV Digest <#{SiteConfig.default_site_email}>" }
 
   def digest_email(user, articles)
+    # takes in a user...
     @user = user
+    # and refines to a collection of 6 articles
     @articles = articles.first(6)
+    # generate an unsubscribe token for the user and the email digest
     @unsubscribe = generate_unsubscribe_token(@user.id, :email_digest_periodic)
+    # create subject for the email using the helper method
     subject = generate_title
+    # mail it!!!!!
     mail(to: @user.email, subject: subject)
   end
 
@@ -21,12 +26,14 @@ class DigestMailer < ApplicationMailer
   end
 
   def random_emoji
+    # get the random emojis, not sure why they would not just sample 3?
     ["🤓", "🎉", "🙈", "🔥", "💬", "👋", "👏", "🐶", "🦁", "🐙", "🦄", "❤️", "😇"].shuffle.take(3).join
   end
 
   def email_end_phrase
     # "more trending DEV posts" won the previous split test
     # Included more often as per explore-exploit algorithm
+    # nice saber metrics ^
     [
       "more trending DEV posts",
       "more trending DEV posts",
