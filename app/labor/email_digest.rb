7d37ff9001e75c4287246d0b2e8b6de64da20b0d
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-22 10:42:31 -0600

    add email_digest annotations

--- app/labor/email_digest.rb
+++ app/labor/email_digest.rb
@@ -12,14 +12,19 @@ class EmailDigest
     @users = users.empty? ? get_users : users
   end
 
+  # sends the users a digest email
   def send_periodic_digest_email
+    # finds users linked to the email digest and for each user sends them the email.
     @users.find_each do |user|
       user_email_heuristic = EmailLogic.new(user).analyze
+      # check if each user is supposed to receive an email if so send it. If not go to next user.
       next unless user_email_heuristic.should_receive_email?
 
       articles = user_email_heuristic.articles_to_send
+      # sends an email
       begin
         DigestMailer.digest_email(user, articles).deliver if user.email_digest_periodic == true
+      # if email fails log an error
       rescue StandardError => e
         Rails.logger.error("Email issue: #{e}")
       end
