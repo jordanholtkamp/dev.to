354085493630875ad427e197ad29a7fc7f0050ae
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-26 16:35:42 -0600

    Adds method for tag names and getting articles with those
    Co-authored-by: Caleb Haizlett <caleb.haizlett@gmail.com>, Lain McGrath <lain.mcgrath@gusto.com>

--- app/labor/daily_email_logic.rb
+++ app/labor/daily_email_logic.rb
@@ -4,8 +4,18 @@ class DailyEmailLogic
   end
 
   def get_article_to_send
-    Article.
+    articles_with_user_tags.
       where.not(user: @user).
+      order(hotness_score: :desc).
       first
+  end
+
+  def get_user_tag_names
+    Tag.where(id: @user.follows.where(followable_type: "ActsAsTaggableOn::Tag").pluck(:followable_id)).pluck(:name)
+  end
+
+  def articles_with_user_tags
+    Article.tagged_with(get_user_tag_names, any: true).
+      where(published: true)
   end
 end
